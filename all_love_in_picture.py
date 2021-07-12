e9da635821a3abce84f19deaec4edf1223b4b2e2
Benjamin <cl1026931058@163.com>
2021-07-12 17:20:10 +0800

    update

--- all_love_in_picture.py
+++ all_love_in_picture.py
@@ -1,20 +1,22 @@
 from PIL import Image, ImageDraw, ImageFont
 
-font_size, text = 7, "我喜欢你！"
-input_img_path = "/Users/MING/Github/love-with-python/girl.jpeg"
-output_img_path = "/Users/MING/Github/love-with-python/girl.png"
+font_size = 20
+text = "林俊杰！"
+img_path = "D:/Code/ALong1108/love-with-python/input.JPG"
 
-img_raw = Image.open(input_img_path)
+img_raw = Image.open(img_path)
 img_array = img_raw.load()
 
 img_new = Image.new("RGB", img_raw.size, (0, 0, 0))
 draw = ImageDraw.Draw(img_new)
-font = ImageFont.truetype('/System/Library/Fonts/PingFang.ttc', font_size)
+font = ImageFont.truetype('C:/Windows/fonts/Simkai.ttf', font_size)
+
 
 def character_generator(text):
     while True:
         for i in range(len(text)):
             yield text[i]
+
 
 ch_gen = character_generator(text)
 
@@ -22,4 +24,4 @@ for y in range(0, img_raw.size[1], font_size):
     for x in range(0, img_raw.size[0], font_size):
         draw.text((x, y), next(ch_gen), font=font, fill=img_array[x, y], direction=None)
 
-img_new.convert('RGB').save(output_img_path)
+img_new.convert('RGB').save("D:/Code/ALong1108/love-with-python/output.jpeg")
